fb572585dbb6cd86e69466c79ac91f8f2f0973a4
Kirmanie L Ravariere <enamrik@gmail.com>
2015-09-21 17:06:48 -0400

    kravariere - added tests for auth module

--- mobile/app/ios/account/__tests__/auth-test.js
+++ mobile/app/ios/account/__tests__/auth-test.js
@@ -1,7 +1,22 @@
 jest.dontMock('../auth');
 
 describe('auth', () => {
-  it('should do something', () => {
-    expect(true).toBe(true);
+  it('should make authentication call to backend', () => {
+    var http = require('../../../network/http');
+    var api = require('../../../config/api');
+
+    var auth = require('../auth');
+    var returnValue = auth.login('username', 'password');
+
+    var url = http.mock.calls[0][0];
+    var payload = http.mock.calls[0][1];
+
+    expect(url).toBe(api.domain + '/login');
+    var credentials = JSON.parse(payload.body);
+    expect(credentials.username).toBe('username');
+    expect(credentials.password).toBe('password');
+    expect(payload.headers['Accept']).toBe('application/json');
+    expect(payload.headers['Content-Type']).toBe('application/json');
+    expect(returnValue).not.toBeNull();
   });
 });
