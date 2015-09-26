737dfa947f904c207b11af489acce4454ceb83bc
Kirmanie L Ravariere <enamrik@gmail.com>
2015-09-26 15:28:44 -0700

    kravariere - allow usage of new es6 and es7 features via babel and react packager. Will need to clear packager cache for each change to babelrc via 'gulp clear-packager-cache

--- mobile/gulpfile.js
+++ mobile/gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var jest = require('jest-cli');
+var os = require('os');
+var fs = require('fs');
+var path = require('path');
 
 var jestConfig = {
   rootDir: 'app'
@@ -13,6 +16,24 @@ gulp.task('test', function(done) {
 
 gulp.task('tdd', function() {
   gulp.watch([ jestConfig.rootDir + "/**/*.js" ], [ 'test' ]);
+});
+
+gulp.task('clear-packager-cache', function () {
+  var tempDir = os.tmpdir();
+
+  var cacheFiles = fs.readdirSync(tempDir).filter(function (fileName) {
+    return fileName.indexOf('react-packager-cache') === 0;
+  });
+
+  cacheFiles.forEach(function (cacheFile) {
+    var cacheFilePath = path.join(tempDir, cacheFile);
+    fs.unlinkSync(cacheFilePath);
+    console.log('Deleted cache: ', cacheFilePath);
+  });
+
+  if (!cacheFiles.length) {
+    console.log('No cache files found!');
+  }
 });
 
 gulp.task('default', ['test']);
