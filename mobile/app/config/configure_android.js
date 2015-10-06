a02fee7d7f7c5d84703cca62ded458d849b49d5c
Kirmanie L Ravariere <enamrik@gmail.com>
2015-10-06 03:11:41 -0500

    kravariere - fix android require path issue for api module. Hard code android simulator IP for now

--- mobile/app/config/configure_android.js
+++ mobile/app/config/configure_android.js
@@ -1,6 +1,6 @@
 module.exports = function() {
-  const api = require('./app/config/api');
-  api.domain = "http://10.0.2.2:4567";
+  const settings = require('../settings');
+  settings["api-domain"] = "http://10.0.2.2:4567";
 
   var device_info = require('./device_info');
   device_info.os = 'android';
